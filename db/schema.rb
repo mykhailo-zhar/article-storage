37bb006000c22b3df9b3923a9ad92e7785541164
ihateb335 <zhar.misha@gmail.com>
2026-06-17 12:13:52 +0300

    Add unique index for wordpress id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_17_081647) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_17_090925) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id", "category_id"], name: "index_article_categories_on_article_id_and_category_id", unique: true
+    t.index ["article_id"], name: "index_article_categories_on_article_id"
+    t.index ["category_id"], name: "index_article_categories_on_category_id"
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "excerpt"
+    t.datetime "published_at"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "wordpress_id"
+    t.string "wordpress_url"
+    t.index ["wordpress_id"], name: "index_articles_on_wordpress_id", unique: true
+  end
+
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -21,5 +42,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_17_081647) do
     t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
+  add_foreign_key "article_categories", "articles"
+  add_foreign_key "article_categories", "categories"
   add_foreign_key "categories", "categories", column: "parent_id", on_update: :cascade, on_delete: :nullify
 end
