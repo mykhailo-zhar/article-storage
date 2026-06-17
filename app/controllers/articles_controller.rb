ebbd2e1a70d75ae50ca4f88eb1dff3964e58e19e
ihateb335 <zhar.misha@gmail.com>
2026-06-17 12:28:15 +0300

    Remove unneccessary actions from articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  FORBIDDEN_ACTIONS = %i[ new create edit destroy ].freeze
+
+  before_action :forbid_mutations, only: FORBIDDEN_ACTIONS
   before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
@@ -12,7 +15,6 @@ class ArticlesController < ApplicationController
 
   # GET /articles/new
   def new
-    @article = Article.new
   end
 
   # GET /articles/1/edit
@@ -21,43 +23,21 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_content }
-        format.json { render json: @article.errors, status: :unprocessable_content }
-      end
-    end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_content }
-        format.json { render json: @article.errors, status: :unprocessable_content }
-      end
-    end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
-    @article.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to articles_path, notice: "Article was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
   end
 
   private
+    def forbid_mutations
+      head :forbidden
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params.expect(:id))
