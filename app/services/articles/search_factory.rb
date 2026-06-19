23186966128d5cb2b2e55309c47fd812ffe811c0
ihateb335 <zhar.misha@gmail.com>
2026-06-19 14:59:48 +0300

    Add search factory service

---
+++ app/services/articles/search_factory.rb
@@ -0,0 +1,40 @@
+module Articles
+  class SearchFactory
+    include Callable
+
+
+    attr_accessor :search_query, :categories, :page, :per_page, :type
+
+    attr_reader :total_pages
+
+
+    def initialize(search_query: "", categories: [], page: 1, per_page: 12, type: :keywords)
+      self.search_query = search_query
+      self.categories = categories
+      self.page = page
+      self.per_page = per_page
+      self.type = type
+    end
+
+    def call
+      service = service_for(type)
+
+      result = service.call
+      @total_pages = service.total_pages
+      result
+    end
+
+    private
+
+    def service_for(type)
+      case type
+      when :keywords
+        SearchKeywords.new(search_query: search_query, categories: categories, page: page, per_page: per_page)
+      when :similar
+        SearchSimilar.new(search_query: search_query, categories: categories, page: page, per_page: per_page)
+      else
+        raise "Invalid search type: #{type}"
+      end
+    end
+  end
+end
