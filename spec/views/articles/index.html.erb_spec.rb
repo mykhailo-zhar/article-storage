dafaeab1b2cd63322f558b734c1dddd29bb02914
ihateb335 <zhar.misha@gmail.com>
2026-06-22 14:51:53 +0300

    Rewrite index.html.erb_spec to use article presenter

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "articles/index", type: :view do
-  before(:each) do
-    assign(:search_query, "MVP")
-    assign(:selected_categories, [ :idea ])
-    assign(:type, :keywords)
-    assign(:page, 1)
-    category = Category.create!(name: "Idea", wordpress_id: 881_417)
-    assign(:articles, [
+  let(:category) { Category.create!(name: "Idea", wordpress_id: 881_417) }
+  let(:articles) do
+    [
       Article.new(
         title: "SaaS MVP Guide",
         excerpt: "<p>Learn how to build a SaaS MVP.</p>",
@@ -16,7 +12,30 @@ RSpec.describe "articles/index", type: :view do
         published_at: DateTime.parse("2026-03-26T15:04:15"),
         categories: [ category ]
       )
-    ])
+    ]
+  end
+  let(:search_query) { "MVP" }
+  let(:type) { :keywords }
+  let(:selected_categories) { [ :idea ] }
+  let(:page) { 1 }
+  let(:total_pages) { 1 }
+
+  let(:presenter) do
+    Articles::ArticlesPresenter.new(
+      articles: articles,
+      search_query: search_query,
+      type: type,
+      selected_categories: selected_categories,
+      page: page,
+      total_pages: total_pages,
+      context: view
+    )
+  end
+
+  before do
+    assign(:presenter, presenter)
+    assign(:search_query, presenter.search_query)
+    assign(:type, presenter.type)
   end
 
   it "renders the search form" do
@@ -39,53 +58,63 @@ RSpec.describe "articles/index", type: :view do
     assert_select "a.article__link[href=?]", "https://example.com/blog/building-a-saas-startup", text: "View the blog post"
   end
 
-  it "does not render a link for unsafe wordpress URLs" do
-    assign(:articles, [
-      Article.new(
-        title: "Unsafe Article",
-        excerpt: "<p>Excerpt</p>",
-        wordpress_url: "javascript:alert(1)",
-        published_at: DateTime.parse("2026-03-26T15:04:15")
-      )
-    ])
+  context "with an unsafe wordpress URL" do
+    let(:articles) do
+      [
+        Article.new(
+          title: "Unsafe Article",
+          excerpt: "<p>Excerpt</p>",
+          wordpress_url: "javascript:alert(1)",
+          published_at: DateTime.parse("2026-03-26T15:04:15")
+        )
+      ]
+    end
 
-    render
+    it "does not render a link" do
+      render
 
-    assert_select "a.article__link", count: 0
+      assert_select "a.article__link", count: 0
+    end
   end
 
-  it "renders a no-results message when articles are empty" do
-    assign(:articles, [])
-    assign(:search_query, "nothing")
+  context "when there are no articles" do
+    let(:articles) { [] }
+    let(:search_query) { "nothing" }
 
-    render
+    it "renders a no-results message" do
+      render
 
-    assert_select "p", text: "No articles found."
+      assert_select "p", text: "No articles found."
+    end
   end
 
-  it "renders centered pagination when more pages are available" do
-    assign(:total_pages, 3)
-    assign(:articles, Array.new(12) do |index|
-      Article.new(
-        title: "Article #{index}",
-        excerpt: "<p>Excerpt #{index}</p>",
-        published_at: DateTime.parse("2026-03-26T15:04:15")
-      )
-    end)
+  context "when more pages are available" do
+    let(:total_pages) { 3 }
+    let(:articles) do
+      Array.new(12) do |index|
+        Article.new(
+          title: "Article #{index}",
+          excerpt: "<p>Excerpt #{index}</p>",
+          published_at: DateTime.parse("2026-03-26T15:04:15")
+        )
+      end
+    end
 
-    render
+    it "renders centered pagination" do
+      render
 
-    assert_select "nav.pagination[aria-label=?]", "Articles pagination"
-    assert_select "form.pagination__page-form[action=?][method=get]", articles_path
-    assert_select "input[name=search][value=?]", "MVP"
-    assert_select "input[name='categories[]'][value=idea]"
-    assert_select "input[name=type][value=keywords]"
-    assert_select "input#page[name=page][type=number][value=?]", "1"
-    assert_select "input#page[min=?]", "1"
-    assert_select "input#page[max=?]", "3"
-    assert_select ".pagination__current", text: /of 3/
-    assert_select "input[type=submit][value=?]", "Go"
-    assert_select "a.pagination__button", text: "Next"
-    assert_select "a.pagination__button", text: "Previous", count: 0
+      assert_select "nav.pagination[aria-label=?]", "Articles pagination"
+      assert_select "form.pagination__page-form[action=?][method=get]", articles_path
+      assert_select "input[name=search][value=?]", "MVP"
+      assert_select "input[name='categories[]'][value=idea]"
+      assert_select "input[name=type][value=keywords]"
+      assert_select "input#page[name=page][type=number][value=?]", "1"
+      assert_select "input#page[min=?]", "1"
+      assert_select "input#page[max=?]", "3"
+      assert_select ".pagination__current", text: /of 3/
+      assert_select "input[type=submit][value=?]", "Go"
+      assert_select "a.pagination__button", text: "Next"
+      assert_select "a.pagination__button", text: "Previous", count: 0
+    end
   end
 end
